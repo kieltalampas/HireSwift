c4dd13faceeb7fe76516c1617f17b6205ad54b74
jaflim <poweruzerz@gmail.com>
2025-05-31 17:06:55 +0800

    MD5 hashing

--- register.php
+++ register.php
@@ -1,51 +1,47 @@
 <?php
 include 'connect.php';
 
-if(isset($_POST['signUp'])){
+if (isset($_POST['signUp'])) {
     $companyName = $_POST['companyName'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $password = password_hash($password, PASSWORD_DEFAULT); // Hash the password
+    $email = trim(strtolower($_POST['email']));
+    $password = trim($_POST['password']);
+    $password = md5($password); 
 
     $checkEmail = "SELECT * FROM users WHERE email='$email'";
     $result = $conn->query($checkEmail);
-    if($result->num_rows > 0){
+    if ($result->num_rows > 0) {
         echo "Email Address Already Exists!";
-    }
-    else{
+    } else {
         $insertQuery = "INSERT INTO users(company_name, email, password)
-                       VALUES ('$companyName', '$email', '$password')";
-        if($conn->query($insertQuery) === TRUE){
-            header("location: index.php");
-        }
-        else{
-            echo "Error:".$conn->error;
+                        VALUES ('$companyName', '$email', '$password')";
+        if ($conn->query($insertQuery) === TRUE) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Error: " . $conn->error;
         }
     }
 }
 
-if(isset($_POST['signIn'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    
-    $sql = "SELECT * FROM users WHERE email='$email'";
+if (isset($_POST['signIn'])) {
+    $email = trim(strtolower($_POST['email']));
+    $password = trim($_POST['password']);
+    $password = md5($password); 
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
     $result = $conn->query($sql);
-    if($result->num_rows > 0){
+    
+    if ($result && $result->num_rows > 0) {
         session_start();
         $row = $result->fetch_assoc();
-        if(password_verify($password, $row['password'])){
-            $_SESSION['email'] = $row['email'];
-            $_SESSION['company_name'] = $row['company_name'];
-            $_SESSION['user_id'] = $row['id'];
-            header("location: homepage.php");
-            exit();
-        }
-        else{
-            echo "Not Found, Incorrect Email or Password";
-        }
-    }
-    else{
-        echo "Not Found, Incorrect Email or Password";
+
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['company_name'] = $row['company_name'];
+        $_SESSION['user_id'] = $row['id'];
+        header("Location: homepage.php");
+        exit();
+    } else {
+        echo "Incorrect Email or Password";
     }
 }
 ?>
