3b779b88a56354a79cd6858a5a800f2877011812
Kiel <ezekieltalampas.c@gmail.com>
2025-05-31 16:41:29 +0800

    update

--- homepage.php
+++ homepage.php
@@ -15,17 +15,7 @@ include("connect.php");
 <body>
     <div style="text-align:center; padding:15%;">
       <p  style="font-size:50px; font-weight:bold;">
-       Hello  <?php 
-       if(isset($_SESSION['email'])){
-        $email=$_SESSION['email'];
-        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
-        while($row=mysqli_fetch_array($query)){
-            echo $row['firstName'].' '.$row['lastName'];
-        }
-       }
-       ?>
-       :)
-      </p>
+       Hello 
       <a href="logout.php">Logout</a>
     </div>
 </body>
--- register.php
+++ register.php
@@ -1,50 +1,51 @@
-<?php 
-
+<?php
 include 'connect.php';
 
 if(isset($_POST['signUp'])){
-    $firstName=$_POST['fName'];
-    $lastName=$_POST['lName'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $password=md5($password);
+    $companyName = $_POST['companyName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password = password_hash($password, PASSWORD_DEFAULT); // Hash the password
 
-     $checkEmail="SELECT * From users where email='$email'";
-     $result=$conn->query($checkEmail);
-     if($result->num_rows>0){
-        echo "Email Address Already Exists !";
-     }
-     else{
-        $insertQuery="INSERT INTO users(firstName,lastName,email,password)
-                       VALUES ('$firstName','$lastName','$email','$password')";
-            if($conn->query($insertQuery)==TRUE){
-                header("location: index.php");
-            }
-            else{
-                echo "Error:".$conn->error;
-            }
-     }
-   
-
+    $checkEmail = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($checkEmail);
+    if($result->num_rows > 0){
+        echo "Email Address Already Exists!";
+    }
+    else{
+        $insertQuery = "INSERT INTO users(company_name, email, password)
+                       VALUES ('$companyName', '$email', '$password')";
+        if($conn->query($insertQuery) === TRUE){
+            header("location: index.php");
+        }
+        else{
+            echo "Error:".$conn->error;
+        }
+    }
 }
 
 if(isset($_POST['signIn'])){
-   $email=$_POST['email'];
-   $password=$_POST['password'];
-   $password=md5($password) ;
-   
-   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
-   $result=$conn->query($sql);
-   if($result->num_rows>0){
-    session_start();
-    $row=$result->fetch_assoc();
-    $_SESSION['email']=$row['email'];
-    header("Location: homepage.php");
-    exit();
-   }
-   else{
-    echo "Not Found, Incorrect Email or Password";
-   }
-
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        session_start();
+        $row = $result->fetch_assoc();
+        if(password_verify($password, $row['password'])){
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['company_name'] = $row['company_name'];
+            $_SESSION['user_id'] = $row['id'];
+            header("location: homepage.php");
+            exit();
+        }
+        else{
+            echo "Not Found, Incorrect Email or Password";
+        }
+    }
+    else{
+        echo "Not Found, Incorrect Email or Password";
+    }
 }
 ?>
